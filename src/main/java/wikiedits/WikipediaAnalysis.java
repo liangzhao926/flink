f843c3b6f6ebcc2b04165434e2edc617b07046e6
Liang Zhao <lzhao@casa-systems.com>
2018-11-28 20:19:59 +0000

    flatmap fat-jar but only in Eclipse

--- src/main/java/wikiedits/WikipediaAnalysis.java
+++ src/main/java/wikiedits/WikipediaAnalysis.java
@@ -1,14 +1,13 @@
 package wikiedits;
 
-import org.apache.flink.api.common.functions.FoldFunction;
-import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
 import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
+import org.apache.flink.util.Collector;
 
 public class WikipediaAnalysis {
 
@@ -17,28 +16,33 @@ public class WikipediaAnalysis {
 		StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
 		DataStream<WikipediaEditEvent> edits = see.addSource(new WikipediaEditsSource());
 		
-		KeyedStream<WikipediaEditEvent, String> keyedEdits = edits
-			    .keyBy(new KeySelector<WikipediaEditEvent, String>() {
-			        @Override
-			        public String getKey(WikipediaEditEvent event) {
-			            return event.getUser();
-			        }
-			    });
-
-		DataStream<Tuple2<String, Long>> result = keyedEdits
-			    .timeWindow(Time.seconds(5))
-			    .fold(new Tuple2<>("", 0L), new FoldFunction<WikipediaEditEvent, Tuple2<String, Long>>() {
-			        @Override
-			        public Tuple2<String, Long> fold(Tuple2<String, Long> acc, WikipediaEditEvent event) {
-			            acc.f0 = event.getUser();
-			            acc.f1 += event.getByteDiff();
-			            return acc;
-			        }
-			    });
+		// 5 seconds tumbling window
+		DataStream<Tuple2<String, Integer>> results = 
+		edits.flatMap(new Extractor())
+		//group by user name, and sum up the diff bytes
+		.keyBy(0)
+		.timeWindow(Time.seconds(5))
+		.sum(1);
 		
-		result.print();
+		results.print();
 
 		see.execute();
+	}
+	
+
+	/**
+	 * Implements the extractor that extract user name and diff bytes from wiki edits. 
+	 * The function takes a WikipediaEditEvent and extract user name and diff bytes into 
+	 * multiple pairs in the form of "(user, bytes)".
+	 */
+	public static final class Extractor implements FlatMapFunction<WikipediaEditEvent, Tuple2<String, Integer>> {
+
+		@Override
+		public void flatMap(WikipediaEditEvent edit, Collector<Tuple2<String, Integer>> out) throws Exception {
+			// TODO Auto-generated method stub
+			out.collect(new Tuple2<String, Integer>(edit.getUser(), edit.getByteDiff()));
+			
+		}
 	}
 
 }
